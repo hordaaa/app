0b292909f23e623b10cbdb49730df85f6d9ec203
Denis Bakulin <dtorkon@yandex.ru>
2025-02-01 17:19:38 +0000

    add cache

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from random import randint
+from redis import Redis
+
 
 app = Flask(__name__)
 
+redis_cli = Redis()
+
 @app.route("/")
 def index():
-    return f"hello world {randint(1, 1000)}"
+    rand_number = randint(1, 10)
+    flag = redis_cli.get(str(rand_number))
+    if not flag: redis_cli.setex(str(rand_number), 60, str(rand_number))
+    return f"{rand_number} cached? {flag}"
